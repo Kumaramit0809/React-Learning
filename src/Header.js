10920ac59f468dfe006ef1695bba6f867e038628
Amit Kumar <kamit62054@gmail.com>
2024-07-30 19:03:32 +0530

    init

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div classname="header">
+        <div className="logo-container">
+            <img className="logo" src="https://cdn6.f-cdn.com/contestentries/1076909/24772837/59737635d5dbe_thumb900.jpg" alt="logo"/>
+        </div>
+
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Cart</li>
+          </ul>
+
+        </div>
+     
+    </div>
+    </div>
+  )
+}
+
+export default Header
